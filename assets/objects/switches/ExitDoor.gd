cc86aa21ffc70baa07742052efae8698031fee80
VladChandran <vladchandran@gmail.com>
2024-11-28 23:37:58 +0530

    Camera + New Map in Vlad_test

---
+++ assets/objects/switches/ExitDoor.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("Closing")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+	
